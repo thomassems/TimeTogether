d8f282603812b8a1170e9c7f7cd9af20d2f6cbd8
Solemann Mateen <solemann.mateen@mail.utoronto.ca>
2024-05-18 12:16:22 -0400

    Bare bones skeleton + reccomendation

---
+++ entities/friend.py
@@ -0,0 +1,26 @@
+
+from enum import Enum
+
+class Priority(Enum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+class Friend:
+    user 
+    priority
+    last_talked_to
+
+    def __init__(self, user, priority):
+        self.user = user
+        self.priority = priority
+        self.last_talked_to = None
+
+    def change_priority(self, priority):
+        self.priority = priority
+    
+    def get_priority(self):
+        return self.priority
+
+    def get_user(self):
+        return self.user
---
+++ entities/user.py
@@ -0,0 +1,77 @@
+class User:
+    id
+    name
+    friends
+    calendar
+    friends_requests
+    event_requests
+    password
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.friends = []
+        self.calendar = {"Saturday":[], "Sunday":[], "Monday":[], "Tuesday":[], "Wednesday":[], "Thursday":[], "Friday":[]}
+        self.friends_requests = []
+        self.event_requests = []
+
+    def change_friend_priority(self, friend, priority):
+        friend.change_priority(priority)
+
+    def add_friend_request(self, user):
+        self.friends_requests.append(user)
+
+    def send_friend_request(self, friend):
+        friend.add_friend_request(self)
+
+    def accept_friend_request(self, friend):
+        self.friends.append(friend)
+        friend.friends.append(self) # potentially problematic
+        self.friends_requests.remove(friend)
+        friend.friends_requests.remove(self)
+
+    def reject_friend_request(self, friend):
+        self.friends_requests.remove(friend)
+    
+    def add_event_request(self, event):
+        self.event_requests.append(event)
+
+    def add_event_to_calendar(self, event):
+        self.calendar[event.day].append(event)
+    
+    def modify_calendar_ics(self, event):
+        # need to implement
+        pass
+
+    def read_calendar_ics(self):
+        # need to implement
+        pass
+    
+    def accept_event_request(self, event):
+        self.event_requests.remove(event)
+        event.participant_accept(self)
+    
+    def get_calendar(self):
+        return self.calendar
+    
+    def get_friends(self): 
+        return self.friends
+    
+    def get_userid(self):    
+        return self.id
+
+    def add_event_to_calendar(self, event):
+        self.add_event_to_calendar(event)
+
+    def reject_event_request(self, event):
+        self.event_requests.remove(event)
+        # need to add a way to show the message this to Event
+        event.participant_reject(self)
+    
+
+    
+   
+    
+    
+
+        
