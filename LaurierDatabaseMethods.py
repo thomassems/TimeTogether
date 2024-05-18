088e4bbdc166083ac9260a80b53b33b4291018eb
thomassems <107775036+thomassems@users.noreply.github.com>
2024-05-18 14:08:48 -0400

    Implemented functions for database

--- LaurierDatabaseMethods.py
+++ LaurierDatabaseMethods.py
@@ -1,8 +1,9 @@
 import psycopg2
+from datetime import datetime
 
 hostname = 'localhost'
 database = 'timetogether'
-username = 'postgres'
+username = 'thomassems'
 pwd = 'admin'
 port_id = 5432
 conn = None
@@ -17,117 +18,208 @@ try:
         cur = conn.cursor()
         def add_user(uID, username):
                 try:
-                        cur.execute("INSERT INTO users (uID, username, friend, lastTalkedTo, priority) VALUES (%s, %s, NULL, NULL, 0)", (uID, username))
+                        cur.execute("""INSERT INTO "User" ("uID", "username", "friend", "lastTalkedTo", "priority") VALUES (%s, %s, NULL, NULL, 0)""", (uID, username))
                         conn.commit()
                         print("User added successfully")
                 except Exception as e:
                         print(e)
                         print("Failed to add user")
-        def add_friend(uID, username, friend, priority):
+
+        # add_user(2, "Nick")
+        # cur.execute("SELECT * FROM \"User\"")
+        # result = cur.fetchall()
+        # print(result)
+
+        conn.commit()
+        def friend_req(uID, username, friend, priority):
                 try:    
-                        cur.execute("INSERT INTO users (uID, username, friend, lastTalkedTo, priority) VALUES (%s, %s, %s, NULL, %s)", (uID, username, friend, priority))
-                        # cur.execute("UPDATE users SET friend = %s WHERE uID = %s", (friend, uID))
+                        cur.execute("""INSERT INTO "User" ("uID", "username", "friend", "lastTalkedTo", "priority") VALUES (%s, %s, %s, NULL, %s)""", (uID, username, friend, priority))
                         conn.commit()
                         print("Friend added successfully")
                 except Exception as e:
                         print(e)
                         print("Failed to add friend")
+
+        # friend_req(3, "Mike", 2, 3)
+        # cur.execute("SELECT * FROM \"User\"")
+        # result = cur.fetchall()
+        # print(result)
+        def accept_req(uID, username, friend): # Very similar to one above it, just set the priority value to 1
+                try:    
+                        cur.execute("""INSERT INTO "User" ("uID", "username", "friend", "lastTalkedTo", "priority") VALUES (%s, %s, %s, NULL, 1)""", (uID, username, friend))
+                        conn.commit()
+                        print("Friend added successfully")
+                except Exception as e:
+                        print(e)
+                        print("Failed to add friend")
+
+        def update_priority(uID, friend, priority): # check that they are friends before-hand
+                try:
+                        cur.execute("""UPDATE "User" SET "priority" = %s WHERE "uID" = %s AND "friend" = %s""", (priority, uID, friend))
+
+        current_timestamp = datetime.now()
+
         def invite_to_event(eID, uID, friendID, eventName, eventTime, eventLocation, eventDescription):
                 try:
-                        cur.execute("INSERT INTO invites (eID, uID, friendID, response) VALUES (%s, %s, %s, False)", (eID, uID, friendID))
-                        #cur.execute("INSERT INTO invites (eID, uID, response) VALUES (%s, %s, NULL)", (eID, uID))
+                        cur.execute("""INSERT INTO "events" ("eID", "eventName", "eventTime", "eventLocation", "eventDescription") VALUES (%s, %s, %s, %s, %s)""", (eID, eventName, eventTime, eventLocation, eventDescription))
                         conn.commit()
-                        cur.execute("INSERT INTO events (eID, eventName, eventTime, eventLocation, eventDescription) VALUES (%s, %s, %s, %s, %s)", (eID, eventName, eventTime, eventLocation, eventDescription))
+                        cur.execute("""INSERT INTO "invites" ("eID", "uID", "friendID") VALUES (%s, %s, %s)""", (eID, uID, friendID))
                         conn.commit()
                         print("Invitation sent successfully")
                 except Exception as e:
                         print(e)
                         print("Failed to send invitation")
+
+        # invite_to_event(2, 3, 2, "Hackathon", current_timestamp, "Milton", "Start coding")
+        # cur.execute("SELECT * FROM \"invites\"")
+        # cur.execute("SELECT * FROM \"events\"")
+        # result = cur.fetchall()
+        # print(result)
+
         def update_event(eID, eventName, eventTime, eventLocation, eventDescription):
                 try:
-                        cur.execute("UPDATE events SET eventName = %s, eventTime = %s, eventLocation = %s, eventDescription = %s WHERE eID = %s", (eventName, eventTime, eventLocation, eventDescription, eID))
+                        cur.execute("""UPDATE "events" SET "eventName" = %s, "eventTime" = %s, "eventLocation" = %s, 
+                                    "eventDescription" = %s WHERE "eID" = %s""", (eventName, eventTime, eventLocation, eventDescription, eID))
                         conn.commit()
                         print("Event updated successfully")
                 except Exception as e:
                         print(e)
                         print("Failed to update event")
+
+        # update_event(1, "Nick's soccer tourny", current_timestamp, "Etobicoke", "Watch Nick get hat trick")
+        # cur.execute("SELECT * FROM \"events\"")
+        # result = cur.fetchall()
+        # print(result)
+        
         def add_participant_to_event(eID, uID):
                 try:
-                        cur.execute("INSERT INTO event_participants (eID, uID) VALUES (%s, %s)", (eID, uID))
+                        cur.execute("""INSERT INTO "event_participants" ("eID", "uID") VALUES (%s, %s)""", (eID, uID))
                         conn.commit()
                         print("Participant added successfully")
                 except Exception as e:
                         print(e)
                         print("Failed to add participant")
-        def accept_invite(eID, friendID): # MAYBE DO A COUPLE EXTRA TRY-CATCH BLOCKS
-                try:
-                        oID = "SELECT uID FROM invites WHERE eID = %s AND friendID = %s", (eID, friendID)
-                        conn.commit()
+
+        # add_participant_to_event(1, 1)
+        # cur.execute("SELECT * FROM \"event_participants\"")
+        # result = cur.fetchall()
+        # print(result)
+
+        def accept_invite(eID, friendID): 
+                try:                        
+                        cur.execute(
+                        """SELECT "uID" FROM "invites" WHERE "eID" = %s AND "friendID" = %s""", (eID, friendID))
+                        result = cur.fetchone()
+                        if result is not None:
+                                oID = result[0]
+                        
                         add_participant_to_event(eID, oID)
                         add_participant_to_event(eID, friendID)
-                        # other person's ID
-                        # cur.execute("UPDATE invites SET response = True WHERE eID = %s AND uID = %s", (eID, uID))
-                        print("Invite accepted successfully")
+                        print("accepted")
+                        
                 except Exception as e:
                         print(e)
                         print("Failed to accept invite")
 
-# GETTERS
+        # accept_invite(2, 2)
+        # cur.execute("SELECT * FROM \"invites\"")
+        # cur.execute("SELECT * FROM \"event_participants\"")
+        # result = cur.fetchall()
+        # print(result)
 
         def get_user_events(uID):
                 try:
-                        cur.execute("SELECT eID, eventName, eventTime, eventLocation, eventDescription FROM events WHERE eID IN (SELECT eID FROM event_participants WHERE uID = %s)", (uID))
+                        cur.execute(
+                        """
+                        SELECT "eID", "eventName", "eventTime", "eventLocation", "eventDescription" FROM "events" WHERE "eID" IN 
+                        (SELECT "eID" FROM "event_participants" WHERE "uID" = %s)""", (uID,))
+        
                         conn.commit()
                         print("Events retrieved successfully")
                         events = []
                         for event in cur:
                                 subevents = []
-                                subevents.append(event['eID'])
-                                subevents.append(event['eventName'])
-                                subevents.append(event['eventTime'])
-                                subevents.append(event['eventLocation'])
-                                subevents.append(event['eventDescription'])
+                                subevents.append(event[0])
+                                subevents.append(event[1])
+                                subevents.append(event[2])
+                                subevents.append(event[3])
+                                subevents.append(event[4])
                                 events.append(subevents)
                         return events
                 except Exception as e:
                         print(e)
                         print("Failed to retrieve events")
+       
+        # print(get_user_events(1))
 
         def get_user_friends(uID):
                 try:
-                        cur.execute("SELECT uID, username, priority FROM users WHERE uID IN (SELECT friend FROM users WHERE uID = %s)", (uID))
+                        cur.execute(
+                        """SELECT "uID", "username", "priority" FROM "User" WHERE "uID" IN (SELECT "friend" 
+                        FROM "User" WHERE "uID" = %s)""", (uID,))
                         conn.commit()
                         print("Friends retrieved successfully")
                         friends = []
                         for friend in cur:
                                 subfriends = []
-                                subfriends.append(friend['uID'])
-                                subfriends.append(friend['username'])
-                                subfriends.append(friend['priority'])
+                                subfriends.append(friend[0])
+                                subfriends.append(friend[1])
+                                subfriends.append(friend[2])
                                 friends.append(subfriends)
+                        return friends
                 except Exception as e:
                         print(e)
                         print("Failed to retrieve friends")
+        
+        print(get_user_friends(3))
 
         def get_user_invites(friendID):
                 try:
-                        cur.execute("SELECT e.eID, e.eventName, e.eventTime, e.eventLocation, e.eventDescription, i.uID, u.username FROM events e JOIN invites i ON e.eID = i.eID JOIN users u ON i.uID = u.uID WHERE i.friendID = %s", (friendID,))
+                        cur.execute(
+                        """
+                        SELECT e."eID", e."eventName", e."eventTime", e."eventLocation", e."eventDescription", i."uID", u."username"
+                        FROM "events" e JOIN "invites" i ON e."eID" = i."eID" JOIN "User" u ON i."uID" = u."uID"
+                        WHERE i."friendID" = %s""", (friendID,))
                         conn.commit()
                         print("Invites retrieved successfully")
                         invites = []
                         for invite in cur:
                                 subinvites = []
-                                subinvites.append(invite['eID'])
-                                subinvites.append(invite['eventName'])
-                                subinvites.append(invite['eventTime'])
-                                subinvites.append(invite['eventLocation'])
-                                subinvites.append(invite['eventDescription'])
-                                subinvites.append(invite['uID'])
-                                subinvites.append(invite['username'])
+                                subinvites.append(invite[0])
+                                subinvites.append(invite[1])
+                                subinvites.append(invite[2])
+                                subinvites.append(invite[3])
+                                subinvites.append(invite[4])
+                                subinvites.append(invite[5])
+                                subinvites.append(invite[6])
                                 invites.append(subinvites)
+                        return invites
                 except Exception as e:
                         print(e)
                         print("Failed to retrieve invites")
+        
+        # print(get_user_invites(2))
+
+        # Create remove functions
+
+        def update_last_talked_to(uID, friendID):
+                try:
+                        cur.execute("""UPDATE "User" SET "lastTalkedTo" = %s WHERE "uID" = %s AND "friend" = %s
+                        """, (current_timestamp, uID, friendID))
+
+                        # Second update query
+                        cur.execute("""UPDATE "User" SET "lastTalkedTo" = %s
+                        WHERE "uID" = %s AND "friend" = %s""", (current_timestamp, friendID, uID))
+                except Exception as e:
+                        print(e)
+                        print("Failed to update last talked to")
+        
+        update_last_talked_to(3, 2)
+        cur.execute("SELECT * FROM \"User\"")
+        result = cur.fetchall()
+        print("hello")
+        print(result)
+
         # Example usage:
         cur.close()
         conn.close()
